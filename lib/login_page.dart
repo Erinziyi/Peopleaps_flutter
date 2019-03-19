8f5772463d61758bf46620bc5c0bec57a1f276c4
unknown <erin@peoplelogy.com>
2019-03-19 18:53:11 +0800

    Change final to Widget

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,94 +1,79 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatefulWidget{
   static String tag ='login-page';
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+ enum FormMode{LOGIN,SIGNUP}
+
+ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    final logo = Hero(
-      tag: 'hero',
-      child: Image.asset('assets/peopleapslogo.png'),
 
-    );
+    Widget _showLogo() {
+      return new Hero(
+        tag: 'hero',
+        child: Image.asset('assets/peopleapslogo.png'),
 
-    final email = TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      autofocus: false,
-      decoration: InputDecoration(
-        hintText: 'Email',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      );
+    }
+    
+    Widget _emailInput(){
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+        child: new TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          autofocus: false,
+          decoration: new InputDecoration(
+            hintText: 'Email',
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
 
-      ),
-    );
+          ),
 
-    final password = TextFormField(
-      autofocus: false,
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: 'Password',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-      ),
-    );
+          validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+          onSaved: (value) => _email = value,
+        )
+      );
+    }
 
+    Widget _paswordInput(){
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+        child: new TextFormField(
+          autofocus: false,
+          obscureText: true,
+          decoration: new InputDecoration(
+            hintText: 'Password',
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+          ),
 
+          validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+          onSaved: (value) => _password = value,
+        )
 
-    final loginButton = Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
+      );
 
-    onPressed: (){
-      var HomePage;
-      Navigator.of(context).pushNamed(HomePage.tag);
-    },
-     padding: EdgeInsets.all(12),
-     color: Colors.lightBlueAccent,
-     child: Text('Log In',style: TextStyle(color: Colors.white)),
-      ),
-    );
-
-
-    final forgotLabel = FlatButton(
-      child: Text(
-        'Forgot password?',
-        style: TextStyle(color: Colors.black54),
-      ),
-      onPressed: () {},
-    );
+    }
 
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(left: 24.0,right:24.0),
-          children: <Widget>[
-            logo,
-            SizedBox(height: 48.0),
-            email,
-            SizedBox(height: 8.0),
-            password,
-            SizedBox(height: 24.0),
-            loginButton,
-            forgotLabel
 
-          ],
-        ),
-
-      ),
-    );
+    Widget _showBody(){
+      return new Container(
+          padding: EdgeInsets.all(16.0),
+          child: new Form(
+            key: _formKey,
+            child: new ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                _showLogo(),
+                _emailInput(),
+                _paswordInput(),
+               
+              ],
+            ),
+          ));
+    }
   }
-
-  }
-
-
+}

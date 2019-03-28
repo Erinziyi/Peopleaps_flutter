4650ecbfc5eb17c69062a88d06fe0085d2dbf136
unknown <erin@peoplelogy.com>
2019-03-28 19:15:32 +0800

    update Dashboars

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -9,11 +9,65 @@ class DashboardPage extends StatelessWidget {
       body: new Stack(
         children: <Widget>[
           new Container(
+            constraints: new BoxConstraints.expand(
+
+            ),
+            alignment: AlignmentDirectional.topStart,
+            padding: new EdgeInsets.only(left: 18.0, top: 40.0),
             decoration: new BoxDecoration(
               image: new DecorationImage(image: new AssetImage('assets/dashboard_background.png'),fit:BoxFit.cover,),
             ),
 
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: <Widget>[
+                new Container(
+                  width:50.0,
+                  height:50.0,
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: new  DecorationImage(image: new AssetImage('assets/profile_pic.png'),
+                    ),
+                  ),
+
+                 child: new Column(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   mainAxisSize: MainAxisSize.min,
+                   crossAxisAlignment: CrossAxisAlignment.start,
+
+                   children: <Widget>[
+                     new Padding(padding: EdgeInsets.symmetric(vertical:3.0),),
+
+
+                      new Text('John Doe',
+                        style: new TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ) ,)
+                   ],
+                 ),
+
+                  
+                )
+
+
+
+
+              ],
+            ),
+
+
+
+            
+
+
+
+
           )
+
+
 
         ],
       ),
